69fd705debc1a4b196aabc6195a189b884c95ab2
Arwa Motiwala <admin@Arwas-MacBook-Air.local>
2025-10-25 10:37:50 +0530

    Triger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+     <link rel="icon" href="/logo.png" />
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
 				<Navbar />
