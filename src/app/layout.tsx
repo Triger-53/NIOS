4abe9e3c188ab6b15cc122cf080ec8bb1b5a5130
Arwa Motiwala <admin@Arwas-MacBook-Air.local>
2025-10-25 11:47:42 +0530

    Triger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
 	title: "NIOS Class 10 in 1 Day",
 	description: "Master your NIOS course with free notes, fast!",
-	icons: [
-		{ rel: "icon", url: "/logo.ico", sizes: "any" },
-		{ rel: "icon", url: "/logo.svg", type: "image/svg+xml" },
-		{ rel: "apple-touch-icon", url: "/logo.png" },
-	],
 }
 
 export default function RootLayout({
@@ -31,6 +26,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+				<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+				<link rel="shortcut icon" href="/favicon/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<meta name="apple-mobile-web-app-title" content="NIOS" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
 				<Navbar />
